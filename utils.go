f0ce9f51f035f95ad0965aa65499dab7a8a8814c
lxy <lxy@landui.com>
2024-04-09 10:30:07 +0800

    增加rds需要的部分工具方法

--- utils.go
+++ utils.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/big"
 	"math/rand"
+	"net"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gogf/gf/v2/frame/g"
@@ -83,4 +86,56 @@ func StringToUint(str string) uint {
 // FormatAmount 格式化金额字符串
 func FormatAmount(amount *big.Float) string {
 	return amount.Text('f', 2)
+}
+
+// ToMap 将结构化数据转为Map
+func ToMap(data interface{}) (map[string]interface{}, error) {
+	m := make(map[string]interface{})
+	j, _ := json.Marshal(data)
+	err := json.Unmarshal(j, &m)
+	if _, ok := m["CreatedAt"]; ok {
+		createTime, _ := time.Parse(time.RFC3339, m["CreatedAt"].(string))
+		m["create_time"] = createTime.Format("2006-01-02 15:04:05")
+	}
+	if _, ok := m["UpdatedAt"]; ok {
+		updateTime, _ := time.Parse(time.RFC3339, m["UpdatedAt"].(string))
+		m["update_time"] = updateTime.Format("2006-01-02 15:04:05")
+	}
+	m["id"] = m["ID"]
+	delete(m, "password")
+	delete(m, "Password")
+	delete(m, "CreatedAt")
+	delete(m, "UpdatedAt")
+	delete(m, "ID")
+	delete(m, "DeletedAt")
+	return m, err
+}
+
+// IsValidIP 验证IP是否合法
+func IsValidIP(ip string) bool {
+	return net.ParseIP(ip) != nil
+}
+
+// ValidateIpList 验证IP列表是否合法
+func ValidateIpList(ipList string) bool {
+	if strings.TrimSpace(ipList) == "" {
+		return false
+	}
+	ips := strings.Split(ipList, "\n")
+	for _, ip := range ips {
+		if strings.TrimSpace(ip) != "" && !IsValidIP(strings.TrimSpace(ip)) {
+			return false
+		}
+	}
+	return true
+}
+
+func RemoveBlankElementInSlice(s []string) []string {
+	var result []string
+	for _, v := range s {
+		if strings.TrimSpace(v) != "" {
+			result = append(result, v)
+		}
+	}
+	return result
 }
